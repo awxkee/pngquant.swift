7d9ea09cf9f0a81107dff5d1e6dd2e6b289b143d
awxkee <qnljoy@gmail.com>
2022-04-27 21:56:08 +0200

    first release

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "pnqquant",
+    name: "pngquant",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "pnqquant",
-            targets: ["pnqquant"]),
+            name: "pngquant",
+            targets: ["pngquant"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "pnqquant",
-            dependencies: [.target(name: "pnqquantc")]),
+            name: "pngquant",
+            dependencies: [.target(name: "pngquantc")]),
         .target(
-            name: "pnqquantc",
+            name: "pngquantc",
             dependencies: [],
             sources: [
                 "blur.c",
@@ -50,6 +50,6 @@ let package = Package(
         ),
         .testTarget(
             name: "pnqquant.swiftTests",
-            dependencies: ["pnqquant"]),
+            dependencies: ["pngquant"]),
     ]
 )
